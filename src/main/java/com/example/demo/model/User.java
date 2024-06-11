134b2865ef9ea936f7abc13729b14915e86fab98
kirankumarkommu007 <kommukirankumar007@gmail.com>
2024-06-11 12:56:58 +0530

    all test methods added

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,19 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+ @Id
+ private Long id;
+ private String name;
+
+ // getters and setters
+}
